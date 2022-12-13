533ddb8ae9f94525b18d8909dcfc3037909735b3
Louis <Louis.Medina@flatironschool.com>
2022-12-13 14:31:13 -0500

    complete header deliverable

--- 02_state_and_events /project_showcase/src/components/Header.js
+++ 02_state_and_events /project_showcase/src/components/Header.js
@@ -1,13 +1,46 @@
-import React from "react";
+// import React from "react";
+
+import { useState } from "react";
 
 const Header = () => {
+  
+  // Initialize isDarkMode State + Setter Function
+
+  // isDarkMode => State
+  // setDarkMode => Setter Function (Automatically Triggers Re-Render)
+  // false => Initial Value for Our State
+
+  // console.log(useState(false));
+
+  // Array Destructuring Assignment
+    // useState() => Returns an Array
+
+  // [ state itself, setter function]
+
+  const [ isDarkMode, setDarkMode ] = useState(false);
+
+  // console.log(isDarkMode);
+
+  const handleClick = () => {
+    setDarkMode(!isDarkMode);
+
+    // 1st Click=> false / true
+    // 2nd Click => true / false
+    // ...
+
+  }
+
   return (
     <header>
       <h1>
         <span className="logo">{"//"}</span>
         Project Showcase
       </h1>
-      <button>Dark Mode</button>
+      <button onClick={handleClick}>
+        {/* CONDITION ? IF TRUE : IF FALSE */}
+        
+        {isDarkMode ? "Dark Mode" : "Light Mode" }
+      </button>
     </header>
   );
 }
